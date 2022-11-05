ad7649f17caf9f0bcfdb1e640ea62cb304765e5b
igor-mota <igormota999@gmail.com>
2022-11-05 06:43:19 -0300

    fix

--- src/common/components/listReports/index.tsx
+++ src/common/components/listReports/index.tsx
@@ -1,24 +1,38 @@
-import MaterialTable from 'material-table'
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
 
 export const ListReports = ({ data }: any) => {
   return (
     <div style={{ maxWidth: '100%' }}>
-      <MaterialTable
-        columns={[
-          { title: 'Numero', field: 'name' },
-          { title: 'Jogo', field: 'surname' },
-          { title: 'E.Inicial', field: 'birthYear' },
-          { title: 'E.Final', field: 'birthCity' },
-          { title: 'T.Entradas', field: 'birthCity' },
-          { title: 'S.Inicial', field: 'birthCity' },
-          { title: 'S.Final', field: 'birthCity' },
-          { title: 'T.Saidas', field: 'birthCity' },
-          { title: 'Resultado', field: 'birthCity' },
-          { title: 'Data', field: 'birthCity' }
-        ]}
-        data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
-        title='Relatorios'
-      />
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Numero</TableCell>
+              <TableCell>Jogo</TableCell>
+              <TableCell>E.Inicial</TableCell>
+              <TableCell>E.Final</TableCell>
+              <TableCell>T.Entradas</TableCell>
+              <TableCell>S.Inicial</TableCell>
+              <TableCell>S.Final</TableCell>
+              <TableCell>T.Saidas</TableCell>
+              <TableCell>Resultado</TableCell>
+              <TableCell>Data</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell>Data</TableCell>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
